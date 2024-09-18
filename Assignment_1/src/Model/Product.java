f6c89f2ee4150b1a9a7def73c6b7236abd29f485
Manasvinikottapally <kottapally.m@northeastern.edu>
2024-09-18 19:34:57 -0400

    Assignment_1 files

---
+++ Assignment_1/src/Model/Product.java
@@ -0,0 +1,193 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author manasvini
+ */
+public class Product {
+    String FirstName;
+    String LastName;
+    String Email;
+    String Phone;
+    String DriversLicenseNumber;
+    String DriversLicenseState;
+    String SocialSecurityNumber;
+    String AddressLine1;
+    String AddressLine2;
+    String City;
+    String State;
+    String ZIP;
+    String TelephoneNumber;
+    String FaxNumber;
+    String University;
+    String Degree;
+    String FieldofStudy;
+    String GPA;
+    String WorkExperience;
+    String Skills;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDriversLicenseNumber() {
+        return DriversLicenseNumber;
+    }
+
+    public void setDriversLicenseNumber(String DriversLicenseNumber) {
+        this.DriversLicenseNumber = DriversLicenseNumber;
+    }
+
+    public String getDriversLicenseState() {
+        return DriversLicenseState;
+    }
+
+    public void setDriversLicenseState(String DriversLicenseState) {
+        this.DriversLicenseState = DriversLicenseState;
+    }
+
+    public String getSocialSecurityNumber() {
+        return SocialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String SocialSecurityNumber) {
+        this.SocialSecurityNumber = SocialSecurityNumber;
+    }
+
+    public String getAddressLine1() {
+        return AddressLine1;
+    }
+
+    public void setAddressLine1(String AddressLine1) {
+        this.AddressLine1 = AddressLine1;
+    }
+
+    public String getAddressLine2() {
+        return AddressLine2;
+    }
+
+    public void setAddressLine2(String AddressLine2) {
+        this.AddressLine2 = AddressLine2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZIP() {
+        return ZIP;
+    }
+
+    public void setZIP(String ZIP) {
+        this.ZIP = ZIP;
+    }
+
+    public String getTelephoneNumber() {
+        return TelephoneNumber;
+    }
+
+    public void setTelephoneNumber(String TelephoneNumber) {
+        this.TelephoneNumber = TelephoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return FaxNumber;
+    }
+
+    public void setFaxNumber(String FaxNumber) {
+        this.FaxNumber = FaxNumber;
+    }
+
+    public String getUniversity() {
+        return University;
+    }
+
+    public void setUniversity(String University) {
+        this.University = University;
+    }
+
+    public String getDegree() {
+        return Degree;
+    }
+
+    public void setDegree(String Degree) {
+        this.Degree = Degree;
+    }
+
+    public String getFieldofStudy() {
+        return FieldofStudy;
+    }
+
+    public void setFieldofStudy(String FieldofStudy) {
+        this.FieldofStudy = FieldofStudy;
+    }
+
+    public String getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(String GPA) {
+        this.GPA = GPA;
+    }
+
+    public String getWorkExperience() {
+        return WorkExperience;
+    }
+
+    public void setWorkExperience(String WorkExperience) {
+        this.WorkExperience = WorkExperience;
+    }
+
+    public String getSkills() {
+        return Skills;
+    }
+
+    public void setSkills(String Skills) {
+        this.Skills = Skills;
+    }
+
+}
